ac03fb8c61b1e0cd3be0cdac6f6d88adc4df9063
Anton <anton333mai@gmail.com>
2024-04-24 17:18:09 +0900

    logo link

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -20,12 +20,25 @@ const Nav = () => {
         <p className="logo_text">Promptopia</p>
       </Link>
 
-      {/*Desctop Navigation*/}
+      {/*Desktop Navigation*/}
       <div className="sm:flex hidden">
         {isUserLoggedIn ? (
           <div className="flex gap-3 md:gap-5">
             <Link href="/create-promt" className="black_btn">
               Create Post
+            </Link>
+            <button type="button" onClick={signOut} className="outline_btn">
+              Sign Out
+            </button>
+
+            <Link href="/profile">
+              <Image
+                src="/assets/images/logo.svg"
+                width={37}
+                height={37}
+                className="rounded-full"
+                alt="profile"
+              />
             </Link>
           </div>
         ) : (
