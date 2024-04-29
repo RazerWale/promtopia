15d3a48557a56abce7c19a2558fa96b0cd057e39
Anton <anton333mai@gmail.com>
2024-04-29 23:25:15 +0900

    route

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -2,6 +2,8 @@ import NextAuth from "next-auth/next";
 import GoogleProvider from "next-auth/providers/google";
 import { connectToDB } from "@utils/database";
 
+import User from "@models/user";
+
 const handler = NextAuth({
   providers: [
     GoogleProvider({
@@ -9,14 +11,27 @@ const handler = NextAuth({
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
     }),
   ],
-  async session({ session }) {},
+  async session({ session }) {
+    const sessionUser = await User.findOne({
+      email: session.user.email,
+    });
+  },
   async signIn({ profile }) {
     try {
       await connectToDB();
 
       // check if a user already exists
-
+      const userExists = await User.findOne({
+        email: profile.email,
+      });
       // if not, create a new user
+      if (!userExists) {
+        await User.create({
+          email: profile.email,
+          username: profile.name.replace(" ", "").toLowerCase(),
+          image: profile.picture,
+        });
+      }
 
       return true;
     } catch (error) {
