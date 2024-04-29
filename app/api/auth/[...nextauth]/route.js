b8e250adb7c361db219c9f0df8575a3426b4a294
Anton <anton333mai@gmail.com>
2024-04-29 23:47:11 +0900

    hotfix

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -15,6 +15,10 @@ const handler = NextAuth({
     const sessionUser = await User.findOne({
       email: session.user.email,
     });
+
+    session.user.id = sessionUser._id.toString();
+
+    return session;
   },
   async signIn({ profile }) {
     try {
